0b8eab7b9aa128edfd19ed64ae34e9ca662fb27f
Ninjaxeplays <82251983+Ephraim163@users.noreply.github.com>
2025-08-10 11:06:48 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
+// next.config.ts
 const repo = "skylog";
 const isProd = process.env.NODE_ENV === "production";
-export default {
+
+const nextConfig = {
   output: "export",
   basePath: isProd ? `/${repo}` : "",
   assetPrefix: isProd ? `/${repo}/` : "",
   images: { unoptimized: true },
-} as const;
+};
+
+export default nextConfig;
