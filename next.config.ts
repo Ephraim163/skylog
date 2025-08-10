cfdf33e5900cd637f89e4b8285c56c4b99ecb0a6
Ninjaxeplays <82251983+Ephraim163@users.noreply.github.com>
2025-08-10 10:36:19 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// next.config.ts
+const repo = "skylog";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",              // static build for GitHub Pages
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+  images: { unoptimized: true }, // required for next/image on Pages
 };
 
 export default nextConfig;
